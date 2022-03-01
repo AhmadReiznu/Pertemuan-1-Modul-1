d5578ace29acc7e9f71c3f27cc2f67b6f00f5fe8
Reiznu Ahmad Tjandrida <55748494+AhmadReiznu@users.noreply.github.com>
2022-03-01 22:16:53 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,36 +1,30 @@
 #include <iostream>
+#include <conio.h>
+
 using namespace std;
-
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
-
-int main() {
-	int data[] = {5, 2, 4, 4, 5};
-	int total = 0;
+int main()
+  {
+    int i, n, data[50];
+    float rata,jum;
     
-    cout << "Nilai ke-1: " << data[0] << endl;
-    cout << "Nilai ke-2: " << data[1] << endl;
-    cout << "Nilai ke-3: " << data[2] << endl;
-    cout << "Nilai ke-4: " << data[3] << endl;
-    cout << "Nilai ke-5: " << data[4] << endl;
+    cout<<"Masukkan Banyaknya Data = ";
+    cin>>n;
+    cout<<endl;
     
-    int panjang_array = sizeof(data) / sizeof(*data);
-    int jumlah = sizeof(data);
-    int rata_rata = jumlah / panjang_array;
+    for (i=0; i<n; i++)
+        {
+           cout<<"Data ["<<i+1<<"] = ";
+           cin>>data[i];
+           jum += data[i];
+        }
+        
+    rata=jum/n;
+        
+    cout<<endl;
     
-    for(int i = 0; i < panjang_array; i++) {
- 
-        total += data[i];
-    }
-    
-    // Menghitung Panjang Array
-    cout << "Banyak Data = " << panjang_array << endl;
-    
-    // Menghitung Rata-Rata
-    cout << "Rata-rata = " << rata_rata << endl;
-    
-    // Menghitung Jumlah Array
-    cout << "Jumlah = " << total;
-    
-    
-	return 0;
-}
+    cout<<"Banyaknya Data : "<<n<<endl;
+    cout<<"Nilai Rata-rata : "<<rata<<endl;
+    cout<<"Nilai Total : "<<jum<<endl;
+	
+  getch();
+  }
